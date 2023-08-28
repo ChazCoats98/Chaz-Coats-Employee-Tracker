a44081c20a2b7a1b514e96f5bf38525272215853
Chaz Coats <chazcoats2@hotmail.com>
2023-08-28 14:37:35 -0400

    department, employee, and role tables laid out

--- db/schema.sql
+++ db/schema.sql
@@ -3,5 +3,20 @@ CREATE TABLE employee_database;
 USE employee_database;
 
 CREATE TABLE department (
-    
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30)
+)
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    )
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT NOT NULL,
 )
