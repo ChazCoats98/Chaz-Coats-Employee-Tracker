3675def98f7a1796b38d4eff5e6f60cea6bb24fe
Chaz Coats <chazcoats2@hotmail.com>
2023-08-29 08:30:31 -0400

    building add role, employee, and department func

--- index.js
+++ index.js
@@ -14,6 +14,42 @@ var dbNav = [
     choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update employee role"]
 }
 ]
+var departmentPrompt = [
+    {name: "newDepartment",
+    type: "input",
+    message: "Type the name of the department"
+}
+];
+var rolePrompt = [
+    {name: "newRole",
+    type: "input",
+    message: "Type the name of the role"
+},
+    {name: "roleSalary",
+    type: "input",
+    message: "What is the salary of the role"
+}
+];
+var employeePrompt = [
+    {name: "firstName",
+    type: "input",
+    message: "What is the first name of the employee"
+},
+    {name: "lastName",
+    type: "input",
+    message: "What is the last name of the employee"
+},
+    {name: "roleName",
+    type: "list",
+    message: "What is the role of this employee",
+    choices: []
+},
+    {name: "managerName",
+    type: "list",
+    message: "Who is the manager of this employee",
+    choices: []
+}
+];
 
 function promptQuestions() {
     inquirer.prompt(dbNav).then(function(data) {
@@ -21,7 +57,7 @@ function promptQuestions() {
             case "view all departments": 
             function viewDepartments() {
                 connection.query("SELECT * FROM departments", function(err, res) {
-                    
+
                 });
             }
             break;
@@ -32,16 +68,28 @@ function promptQuestions() {
 
             break;
             case "add a department":
+            function newDepartment() {
+                inquirer.prompt(departmentPrompt).then(function(data) {
+                    connection.query("INSERT INTO department (name) VALUES (?)", function(err, res) {
 
+                    });
+                });
+            }
             break;
             case "add a role":
-
+            function newRole() {
+                inquirer.prompt(rolePrompt).then(function(data) {});
+            };
             break;
             case "add an employee":
-                
+            function newEmployee() {
+                inquirer.prompt(employeePrompt).then(function(data) {});
+            };
             break;
             case "update employee role":
-
+            function updateRole() {
+                inquirer.prompt(rolePrompt).then(function(data) {});
+            };
             break; 
         }
     });
