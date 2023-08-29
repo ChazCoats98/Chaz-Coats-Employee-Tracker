36c57e95cef01254332cb6e079650c7e1bcf51f2
Chaz Coats <chazcoats2@hotmail.com>
2023-08-29 15:13:28 -0400

    adding seeds

--- db/schema.sql
+++ db/schema.sql
@@ -4,19 +4,22 @@ USE employee_database;
 
 CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30)
-)
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    )
+    role_id INT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+    );
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT NOT NULL,
-)
+    department_id INT,
+    PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,11 @@
 INSERT INTO departments (name)
-values ("CEO"),
-("CFO"),
-("Department Lead"),
-("Software Engineer"),
-("Junior Web Developer")
-INSERT INTO employees(id, title, first_name, last_name, role_id, manager_id) 
+values ("Management"),
+("Software Development"),
+("Human Resources"),
+("operations"),
+("Interns");
+INSERT INTO employees(first_name, last_name, role_id, manager_id) 
+values ("Sir John", "Franklin", 1, 1)
+
+INSERT INTO roles(title, salary, department_id)
 values
-()
