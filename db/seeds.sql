6dd1acc6c48920836f5d5a8da6d58153d31f7cec
Chaz Coats <chazcoats2@hotmail.com>
2023-08-30 13:03:02 -0400

    Roles, employees, and departments tables done

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,21 +5,21 @@ values ("Management"),
 ("operations"),
 ("Interns");
 INSERT INTO employees(first_name, last_name, role_id, manager_id) 
-values ("J. Edgar", "Hoover", 1, 1)
+value ("J. Edgar", "Hoover", 1, 1),
 ("John", "Franklin", 2, 1),
-("John", "Lee", 2, 1),
-("Raoul", "Agent", 2, 1),
-("Sonia", "Marmeladov", 2, 3),
-("Hal", "Incandenza", 2, 2),
-("Geordie", "Greep", 3, 3),
-("Mary", "Hogarth", 4, 3),
-("James", "Ray", 5, 4);
+("John", "Lee", 3, 1),
+("Raoul", "Agent", 4, 1),
+("Sonia", "Marmeladov", 5, 3),
+("Hal", "Incandenza", 6, 2),
+("Geordie", "Greep", 7, 3),
+("Mary", "Hogarth", 8, 3),
+("James", "Ray", 9, 4);
 
 
 INSERT INTO roles(title, salary, department_id)
-values ("CEO", 1200000, 1),
+value ("CEO", 1200000, 1),
 ("Research Manager", 800000, 1),
-("Operations and Development Manager", 600000, 1),
+("Development Manager", 600000, 1),
 ("Manager of Outside Resources", 500000, 1),
 ("Software Engineer", 130000, 2),
 ("Future Technologies Engineer", 190000, 2),
